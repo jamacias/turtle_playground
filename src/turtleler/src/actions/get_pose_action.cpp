c57f96474e8078ca055003fdf7e972df14a8a9b1
Javier Macias Sola <jamaciastf@gmail.com>
2026-05-26 14:52:19 +0200

    Move definitions to cpp

--- src/turtleler/src/actions/get_pose_action.cpp
+++ src/turtleler/src/actions/get_pose_action.cpp
@@ -1,9 +1,20 @@
 #include "get_pose_action.hpp"
 
 #include "behaviortree_ros2/plugins.hpp"
+
 #include <algorithm>
 #include <behaviortree_cpp/basic_types.h>
 #include <rclcpp/logging.hpp>
+
+GetPoseAction::GetPoseAction(const std::string& name, const NodeConfig& conf, const RosNodeParams& params)
+: RosTopicSubNode<TFMessage>(name, conf, params)
+{
+}
+
+PortsList GetPoseAction::providedPorts()
+{
+    return providedBasicPorts({OutputPort<Pose>("current_pose", "The current pose.")});
+}
 
 NodeStatus GetPoseAction::onTick(const std::shared_ptr<TFMessage>& lastMsg)
 {
@@ -12,10 +23,9 @@ NodeStatus GetPoseAction::onTick(const std::shared_ptr<TFMessage>& lastMsg)
     if (!lastMsg)
         return NodeStatus::FAILURE;
 
-    const auto tf = std::find_if(lastMsg->transforms.cbegin(), lastMsg->transforms.cend(), [](const geometry_msgs::msg::TransformStamped& tf)
-    {
-        return tf.child_frame_id == "turtle1" && tf.header.frame_id == "world";
-    });
+    const auto tf = std::find_if(lastMsg->transforms.cbegin(), lastMsg->transforms.cend(),
+                                 [](const geometry_msgs::msg::TransformStamped& tf)
+                                 { return tf.child_frame_id == "turtle1" && tf.header.frame_id == "world"; });
 
     if (tf == lastMsg->transforms.cend())
         return NodeStatus::FAILURE;
--- src/turtleler/src/actions/get_pose_action.hpp
+++ src/turtleler/src/actions/get_pose_action.hpp
@@ -6,22 +6,16 @@
 #include <tf2_msgs/msg/tf_message.hpp>
 
 using TFMessage = tf2_msgs::msg::TFMessage;
-using Pose = geometry_msgs::msg::TransformStamped;
+using Pose      = geometry_msgs::msg::TransformStamped;
 
 using namespace BT;
 
 class GetPoseAction : public RosTopicSubNode<TFMessage>
 {
 public:
-    GetPoseAction(const std::string& name, const NodeConfig& conf, const RosNodeParams& params)
-    : RosTopicSubNode<TFMessage>(name, conf, params)
-    {
-    }
+    GetPoseAction(const std::string& name, const NodeConfig& conf, const RosNodeParams& params);
 
-    static PortsList providedPorts()
-    {
-        return providedBasicPorts({OutputPort<Pose>("current_pose", "The current pose.")});
-    }
+    static PortsList providedPorts();
 
     virtual NodeStatus onTick(const std::shared_ptr<TFMessage>& lastMsg) override;
 };
